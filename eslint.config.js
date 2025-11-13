a011c2380e102ce71eba08fd9ea38527e52c2303
x3imal <x3imal@bk.ru>
2025-11-13 11:29:02 +0300

    new-setup-init: добавлены линтеры, библиотеки axios и описание в README.md

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    globalIgnores(["dist", "node_modules"]),
+
+    {
+        files: ["**/*.{js,jsx}"],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+            prettierConfig // отключает все конфликтующие правила
+        ],
+
+        languageOptions: {
+            ecmaVersion: "latest",
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: { jsx: true },
+                sourceType: "module",
+            },
+        },
+
+        rules: {
+            "no-unused-vars": ["warn", { varsIgnorePattern: "^[A-Z_]" }],
+            "no-console": "off",
+            "react/prop-types": "off",
+            "react/react-in-jsx-scope": "off"
+        },
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+]);
