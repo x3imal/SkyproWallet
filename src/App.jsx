7d3ed9d9e76ff8766a7e6e2a65d888ece3096609
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-11-14 21:22:35 +0300

    Created a file ExpenseTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { TestFormat } from './TestFormat.jsx';
+import { ExpenseTable } from './components/ExpenseTable.jsx';
 
 function App() {
   return (
     <>
       <TestFormat />
+      <ExpenseTable />
     </>
   );
 }
---
+++ src/components/ExpenseTable.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const TableItem = styled.div``;
+
+
+
+export const ExpenseTable = () => {
+    return (
+        <>
+        <TableItem>
+            <div>
+                <h2>Таблица расходов</h2>
+            </div>
+            <div>
+                
+            </div>
+        </TableItem>
+        </>
+    )
+}
