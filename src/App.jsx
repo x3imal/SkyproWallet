b6c408ef0dd8b0dbc77365019d158d3cfc824a50
aasur <x3imal@bk.ru>
2025-11-19 18:27:48 +0300

    expenses-chart-modal: добавили модалку "расход"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import {Layout} from "./pages/MainPage.jsx";
+import {ExpensesChartModal} from "./components/ExpensesChartModal/ExpensesChartModal.jsx";
+import {AnalysisPage} from "./pages/AnalysisPage.jsx";
 
 function App() {
   return (
     <>
-      <Layout />
+      <Layout>
+          <AnalysisPage />
+      </Layout>
     </>
   );
 }
