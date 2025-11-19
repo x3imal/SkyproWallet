1e15f3bd5d95a5874cfc6e814c7e7c781ceef05d
alexeyasurov <aasurov@gmail.com>
2025-11-19 10:46:07 +0300

    expense-table: слили нужны ветки

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import {Layout} from "./pages/MainPage.jsx";
+import { ExpenseTable } from './components/ExpenseTable.jsx';
 
 function App() {
   return (
     <>
       <Layout />
+      <ExpenseTable />
     </>
   );
 }
