d90f6bd85b6a0531ddf32b6aca45fece0ce7db51
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-11-19 22:14:23 +0300

    Completed the upper part of the calendar

--- src/components/ExpensesChartModal/Calendar.jsx
+++ src/components/ExpensesChartModal/Calendar.jsx
@@ -12,6 +12,10 @@ export const CalendarHeader = styled.div``;
 
 export const CalendarHeaderTop = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding-bottom: 30px;
+  padding: 32px 32px 30px 32px;
 `;
 
 export const CalendarHeaderTitle = styled.div`
@@ -25,11 +29,46 @@ export const CalendarHeaderTitle = styled.div`
   }
 `;
 
-export const CalendarHeaderPeriods = styled.div``;
+export const CalendarHeaderPeriods = styled.div`
+  display: flex;
+  gap: 12px;
+`;
 
-export const CalendarPeriod = styled.div``;
+export const CalendarPeriod = styled.div`
+  p {
+    color: rgba(0, 0, 0, 1);
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 150%;
+  }
+`;
+
+export const CalendarWeekendsDays = styled.div`
+  display: flex;
+  gap: 32px;
+  justify-content: center;
+`;
+
+export const WeekendsDay = styled.div`
+  h4 {
+    color: rgba(153, 153, 153, 1);
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 15px;
+    letter-spacing: 0%;
+    text-align: center;
+  }
+`;
+
+export const HeaderLine = styled.div`
+  margin-top: 6px;
+  margin-bottom: 24px;
+  border: 0.5px solid rgba(153, 153, 153, 1);
+`;
 
 export const Calendar = () => {
+  const WeekendsDays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+
   return (
     <>
       <CalendarWrapper>
@@ -47,6 +86,14 @@ export const Calendar = () => {
               </CalendarPeriod>
             </CalendarHeaderPeriods>
           </CalendarHeaderTop>
+          <CalendarWeekendsDays>
+            {WeekendsDays.map((day) => (
+              <WeekendsDay key={day}>
+                <h4>{day}</h4>
+              </WeekendsDay>
+            ))}
+          </CalendarWeekendsDays>
+          <HeaderLine />
         </CalendarHeader>
       </CalendarWrapper>
     </>
