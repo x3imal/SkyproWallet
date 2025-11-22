8d643134045a6dc5f5af4886d2fe047b38ed8b5a
x3imal <124359692+x3imal@users.noreply.github.com>
2025-11-22 23:00:33 +0300

    Revert "Expenses chart modal"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import {Layout} from "./pages/MainPage.jsx";
-import {ExpensesChartModal} from "./components/ExpensesChartModal/ExpensesChartModal.jsx";
-import {AnalysisPage} from "./pages/AnalysisPage.jsx";
+import { TestFormat } from './TestFormat.jsx';
 
 function App() {
   return (
     <>
-      <Layout>
-          <AnalysisPage />
-      </Layout>
+      <TestFormat />
     </>
   );
 }
